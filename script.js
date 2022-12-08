a3740547bcc7e222e773ac0b928e7aa480d85ba9
germanytothetop <e272696@edu.sbl.ch>
2022-12-08 11:17:45 +0100

    Aufgabe-02 fertig

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 // Kommentiere hier (b)
-// 
+// Animation wird gestartet, wenn running richtig ist.
 const svg = document.querySelector("#svg-field")
 
 let running = false
@@ -13,22 +13,22 @@ function start() {
 function animationLoop() {
     
     // Kommentiere hier (c)    
-    // 
+    // Der svg-Block von circle wird als const angezeigt
     const dot = document.createElementNS("http://www.w3.org/2000/svg", "circle")
     
     // Kommentiere hier (d)
-    // 
+    // Die Koordinaten, in welchen sich der circle bewegen sollte auch die optische Anzeige des circle 
     let x = Math.random()
     let y = Math.random()
     dot.setAttribute("cx", x)
     dot.setAttribute("cy", y)
     dot.setAttribute("r", 0.01)
     dot.setAttribute("fill", "black")
-    // Kommentiere hier (e)
+    // Kommentiere hier (e) Das svg von circle wird verlinked.
     svg.appendChild(dot)
 
     // Kommentiere hier (f)
-    // 
+    // Wenn running richtig ist, soll die aniamtion abgespielt werden.
     if ( running === true ) {
         window.requestAnimationFrame(animationLoop)
     }
