b4b91d902831c27741811b0227c7248a2c5c1317
Florin Imeri <e249786@edu.sbl.ch>
2022-12-08 11:47:46 +0100

    Kommentare

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 
 // Kommentiere hier (b)
-// 
-const svg = document.querySelector("#svg-field")
+// Es zeigt eine globale Variable
 
 let running = false
 
+const svg = document.querySelector("#svg-field")
 function start() {
     running = true
     window.requestAnimationFrame(animationLoop)
 }
 
+
+
+
+
 function animationLoop() {
     
     // Kommentiere hier (c)    
-    // 
+    // Es ist wieder eine Globale Variable, es zählt immer.
     const dot = document.createElementNS("http://www.w3.org/2000/svg", "circle")
     
     // Kommentiere hier (d)
-    // 
+    // Let zeigt die Definition für x und y.
     let x = Math.random()
     let y = Math.random()
     dot.setAttribute("cx", x)
@@ -25,10 +29,11 @@ function animationLoop() {
     dot.setAttribute("r", 0.01)
     dot.setAttribute("fill", "black")
     // Kommentiere hier (e)
+    // Man hengt die oberen programmierungen im svg an.
     svg.appendChild(dot)
 
     // Kommentiere hier (f)
-    // 
+    // if stellt eine Bedingung klar
     if ( running === true ) {
         window.requestAnimationFrame(animationLoop)
     }
