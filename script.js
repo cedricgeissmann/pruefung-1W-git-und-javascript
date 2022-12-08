2531200fa574a6dbca0d20c69c9a3f303419d1ae
BKK <christian.kuenzi55@gmail.com>
2022-12-08 11:03:38 +0100

    2 abcdef

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 // Kommentiere hier (b)
-// 
+// Es wählt das dokument svg-field aus 
 const svg = document.querySelector("#svg-field")
 
 let running = false
@@ -13,11 +13,11 @@ function start() {
 function animationLoop() {
     
     // Kommentiere hier (c)    
-    // 
+    // Es erstell eine Form die ein Kreis ist 
     const dot = document.createElementNS("http://www.w3.org/2000/svg", "circle")
     
     // Kommentiere hier (d)
-    // 
+    // Nimmt für x eine irgend eine Zahl 
     let x = Math.random()
     let y = Math.random()
     dot.setAttribute("cx", x)
@@ -25,10 +25,11 @@ function animationLoop() {
     dot.setAttribute("r", 0.01)
     dot.setAttribute("fill", "black")
     // Kommentiere hier (e)
+    //Das hängt es in den SVG ein also den Punkt (dot)
     svg.appendChild(dot)
 
     // Kommentiere hier (f)
-    // 
+    // running wird verglichen mit true
     if ( running === true ) {
         window.requestAnimationFrame(animationLoop)
     }
