9b0f38e331faf45364770e54637deceb8860bebb
Aromat06 <lennywuersten6@gmail.com>
2022-12-08 11:11:48 +0100

    2b,c,d,e,f,

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 // Kommentiere hier (b)
-// 
+// Auswahl von Elementen innerhalb anderer festgelegter Bereiche
 const svg = document.querySelector("#svg-field")
 
 let running = false
@@ -13,11 +13,11 @@ function start() {
 function animationLoop() {
     
     // Kommentiere hier (c)    
-    // 
+    // fester, nicht veränderbarer Wert
     const dot = document.createElementNS("http://www.w3.org/2000/svg", "circle")
     
     // Kommentiere hier (d)
-    // 
+    // setAttribute steht für ein Attribut (verändert Werte)
     let x = Math.random()
     let y = Math.random()
     dot.setAttribute("cx", x)
@@ -25,10 +25,11 @@ function animationLoop() {
     dot.setAttribute("r", 0.01)
     dot.setAttribute("fill", "black")
     // Kommentiere hier (e)
+    // alles mit "dot." wird schwarz ausgefüllt
     svg.appendChild(dot)
 
     // Kommentiere hier (f)
-    // 
+    // wenn es true (wahr) ist, dann soll etwas rennen (ausgeführt werden)
     if ( running === true ) {
         window.requestAnimationFrame(animationLoop)
     }
