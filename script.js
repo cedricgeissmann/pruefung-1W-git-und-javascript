cbfee8fb9c986710bcc3ca944db435eee5980939
liliannemari <272705@sbl.ch>
2022-12-08 11:25:42 +0100

    3te changes codes erklären

--- script.js
+++ script.js
@@ -1,46 +1,11 @@
 
-damit die Funktion stimmt svg code einfügen.
-const svg = document.querySelector("#svg-field")
-
-let running = false
-
-function start() {
-    running = true
-    window.requestAnimationFrame(animationLoop)
-}
-
-function animationLoop() {
-    
-    animierter Luping    
-    const dot = document.createElementNS("http://www.w3.org/2000/svg", "circle")
-    
-    kreis aus svg dokument hizugefügt.
-    let x = Math.random()
-    let y = Math.random()
-    dot.setAttribute("cx", x)
-    dot.setAttribute("cy", y)
-    dot.setAttribute("r", 0.01)
-    dot.setAttribute("fill", "black")
-    
-    svg.appendChild(dot)
-
-    // Kommentiere hier (f)
-    // 
-    if ( running === true ) {
-        window.requestAnimationFrame(animationLoop)
-    }
-}
-
-
-
-
 //Eine eckige Fläche mit etwas das rennt und die funktion ist richtig.
 // 
 const svg = document.querySelector("#svg-field")
 
 let running = false
 
-function start() {
+function start(STARK) {
     running = true
     window.requestAnimationFrame(animationLoop)
 }
